71e920f30c25d04106e430f847dba9e5ad72d5bf
SY <seyav29@gmail.com>
2023-05-20 00:52:15 -0400

    first commit

---
+++ src/components/Topbar/Topbar.jsx
@@ -0,0 +1,15 @@
+import "./topbar.css"
+
+const Topbar = () => {
+  return (
+    <div className='topbar'>
+      <div className="topbarWrapper">
+        <div className="topLeft"></div>
+        <div className="top right"></div>
+tt
+      </div>
+    </div>
+  )
+}
+
+export default Topbar
