96119b0718f5a0d28af5560f1938f31b8fc32383
SY <seyav29@gmail.com>
2023-05-21 01:31:50 -0400

    .

--- src/pages/UserPage/User.jsx
+++ src/pages/UserPage/User.jsx
@@ -8,7 +8,22 @@ const User = () => {
         <button className="userAddButton">Create</button>
       </div>
       <div className="userContainer">
-        <div className="userShow">Stan Paul</div>
+        <div className="userShow">
+          <div className="userShowTop">
+            <img
+              src="https://st3.depositphotos.com/22025564/37708/i/600/depositphotos_377086496-stock-photo-black-woman-lifestyle-illustration-african.jpg"
+              alt=""
+              className="userShowImg"
+            />
+            <div className="userShowTopTitle">
+              <span className="userShowUsername">Kwizer Suzer</span>
+              <span className="userShowUserTitle">Developer</span>
+            </div>
+          </div>
+          <div className="userShowBottom">
+            <span className="userShowTitle">Account details</span>
+          </div>
+        </div>
         <div className="userUpdate">Developer</div>
       </div>
     </div>
